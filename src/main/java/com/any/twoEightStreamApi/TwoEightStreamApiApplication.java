3cfa0c5b1ac5b828ca18e0eaf50d10fd0e93ef2e
dispersion <anyta1901@gmail.com>
2022-03-08 23:43:38 +0300

    Initial commit

---
+++ src/main/java/com/any/twoEightStreamApi/TwoEightStreamApiApplication.java
@@ -0,0 +1,13 @@
+package com.any.twoEightStreamApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoEightStreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoEightStreamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/any/twoEightStreamApi/TwoEightStreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.any.twoEightStreamApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoEightStreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
